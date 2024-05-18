9766775d9488185194a26404f0ad92532ac01a02
allanclrolemberg <68305172+allanclrolemberg@users.noreply.github.com>
2024-05-17 23:31:37 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -2,7 +2,7 @@
 const projects = [
     {
         title: "Kukoos: Lost Pets",
-        platforms: ["Icons/PS.png", "icons/xbox.png"],
+        platforms: ["Icons/PS.png", "Icons/windows.png", "Icons/nintendo.png"],
         description: "Descubra o universo dos Kukoos neste incrível jogo de plataforma 3D. Explore mundos misteriosos e descubra mistérios para libertar os pets que sofreram lavagem cerebral. Conheça grandes personagens, novas mecânicas, lugares malucos e divirta-se a valer!",
         category: "Multiplayer",
         images: ["imagem1.jpg", "imagem2.jpg"],
